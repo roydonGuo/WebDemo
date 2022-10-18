7eca2e06b0b33ef6dcc718af8ee2e0245ed60f30
roydonGuo <3133010060@qq.com>
2022-10-18 17:31:12 +0800

    ✨ NEW FEATURE:

--- girl-video1/src/main.js
+++ girl-video1/src/main.js
@@ -4,11 +4,6 @@ import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-// axios.interceptors.request.use((config) => {// 获取token
-//   config.headers.Authorization = window.sessionStorage.getItem('token')
-//   return config
-// })
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
